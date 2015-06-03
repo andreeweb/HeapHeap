0421af67de3e5e1bc975dee08243149fe9a0f932
Andrea Cerra <andrea.cerra@me.com>
2015-06-03 11:28:55 +0200

    Added methods in DHeap class

---
+++ DHeap/DHeap.py
@@ -0,0 +1,25 @@
+__author__ = 'Andrea'
+
+class DHeap:
+
+    def __init__(self, d):
+        self.d = d
+        self.heap = []
+
+    #def find_min:
+        #TODO
+
+    #def insert:
+        #TODO
+
+    #def delete(self, e):
+        #TODO
+
+    #def decrease_key(self, e, k):
+        #TODO
+
+    #def increase_key(self, e, d):
+        #TODO
+
+    #def merge(self):
+        #TODO
