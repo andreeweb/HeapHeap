a461e8ed7d348fcca6d50e1e241cd22c65736292
Andrea Cerra <andrea.cerra@me.com>
2015-06-22 14:09:30 +0200

    un-hide delete method and remove main

--- Heap/DHeap.py
+++ Heap/DHeap.py
@@ -1,7 +1,5 @@
 __author__ = 'Andrea'
 
-import time
-
 from Heap.HeapNode import HeapNode
 
 class DHeap:
@@ -21,15 +19,15 @@ class DHeap:
             return True
         return False
 
-    # def delete(self, node):
-    #    if len(self.heap) == 0:
-    #        return None
-    #    node_to_delete = self.heap[node.index]
-    #    node_leaf = self.heap[- 1]
-    #    self.swap_node(node_to_delete, node_leaf)
-    #    del self.heap[-1]
-    #    self.move_up(node_leaf)
-    #    self.move_down(node_leaf)
+    def delete(self, node):
+        if len(self.heap) == 0:
+            return None
+        node_to_delete = self.heap[node.index]
+        node_leaf = self.heap[- 1]
+        self.swap_node(node_to_delete, node_leaf)
+        del self.heap[-1]
+        self.move_up(node_leaf)
+        self.move_down(node_leaf)
 
     def delete_min(self):
         if len(self.heap) == 0:
@@ -86,77 +84,3 @@ class DHeap:
             n = self.heap[i]
             s += "[{} - {},{}] ".format(i, n.elem, n.key)
         print(s)
-
-if __name__ == "__main__":
-
-    start = time.time()
-
-    pq = DHeap(4)
-
-    if pq.is_empty():
-        print("Empty")
-
-    e = 4.0
-    k = 2.0
-    print("insert({},{})".format(e, k))
-    pq.insert(e, k)
-    pq.print_heap()
-    print("findMin():", pq.get_min())
-
-    e = 2.0
-    k = 1.0
-    print("insert({},{})".format(e, k))
-    pq.insert(e, k)
-    pq.print_heap()
-    print("findMin():", pq.get_min())
-
-    e = 8.0
-    k = 4.0
-    print("insert({},{})".format(e, k))
-    pq.insert(e, k)
-    pq.print_heap()
-    print("findMin():", pq.get_min())
-
-    e = 10.0
-    k = 5.0
-    print("insert({},{})".format(e, k))
-    pq.insert(e, k)
-    pq.print_heap()
-    print("findMin():", pq.get_min())
-
-    e = 6.0
-    k = 3.0
-    print("insert({},{})".format(e, k))
-    pq.insert(e, k)
-    pq.print_heap()
-    print("findMin():", pq.get_min())
-
-    print("deleteMin()")
-    pq.delete_min()
-    pq.print_heap()
-    print("findMin():", pq.get_min())
-
-    e = 12.0
-    k = 6.0
-    print("insert({},{})".format(e, k))
-    pq.insert(e, k)
-    pq.print_heap()
-
-    e = 14.0
-    k = 7.0
-    print("insert({},{})".format(e, k))
-    pq.insert(e, k)
-    pq.print_heap()
-
-    e = 16.0
-    k = 8.0
-    print("insert({},{})".format(e, k))
-    pq.insert(e, k)
-    pq.print_heap()
-
-    print("deleteMin()")
-    pq.delete_min()
-    pq.print_heap()
-
-    end = time.time()
-    print(str(end-start))
