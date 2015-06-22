36c581de2644bdeaa8619507cc000c7000b5193b
Andrea Cerra <andrea.cerra@me.com>
2015-06-22 10:53:17 +0200

    Added __str__ and __repr___ methods in HeapNode class

--- Heap/HeapNode.py
+++ Heap/HeapNode.py
@@ -6,3 +6,9 @@ class HeapNode:
         self.elem = elem
         self.key = key
         self.index = index
+
+    def __str__(self):
+        return str(self.key)
+
+    def __repr__(self):
+        return str(self.key)
