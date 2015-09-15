64e0178e6b2fd7d973dbb9ec623efebc457e0040
Andrea Cerra <andrea.cerra@me.com>
2015-09-16 01:17:28 +0200

    Changed parameter name

--- Utils/Generator.py
+++ Utils/Generator.py
@@ -4,7 +4,7 @@ import random
 class Generator:
 
     @classmethod
-    def generate(cls, seed, count):
+    def generate(cls, seed, input_size):
         """
         Generate count random numbers with the seed specified
 
@@ -14,9 +14,9 @@ class Generator:
         """
         random.seed(seed)
         random_numbers = []
-        for i in range(count):
-            k = random.randint(0, count)
-            j = random.randint(0, count)
+        for i in range(input_size):
+            k = random.randint(0, input_size)
+            j = random.randint(0, input_size)
             random_numbers.append([k, j])
         return random_numbers
 
